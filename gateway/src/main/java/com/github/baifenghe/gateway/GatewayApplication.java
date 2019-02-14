b059062929d932a04e75f666524d4dfcf073f26e
baifenghe <1170126481@qq.com>
2019-02-14 16:27:31 +0800

    :sparkles: add gateway

---
+++ gateway/src/main/java/com/github/baifenghe/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.github.baifenghe.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author bfh
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
+
